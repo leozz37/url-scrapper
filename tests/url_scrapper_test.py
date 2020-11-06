478c12b153eb242e7561d3f569c459b4431c6397
Leonardo Lima <leonardoaugusto287@gmail.com>
2020-11-06 20:49:40 -0300

    ✅ new unit test for spaces

--- tests/url_scrapper_test.py
+++ tests/url_scrapper_test.py
@@ -15,7 +15,17 @@ def scrapper() -> Scrapper:
 
 def test_create_url_with_success(scrapper: Scrapper) -> None:
     """
-    Test if the right URL will be returned
+    Test if the right URL will be returned correctly
+    """
+    expected_output = "https://www.google.com/search?tbm=isch&q=Hello"
+    resulted_output = scrapper.create_url("Hello")
+
+    assert expected_output == resulted_output
+
+
+def test_create_url_without_spaces_with_success(scrapper: Scrapper) -> None:
+    """
+    Test if the right URL will be returned without spaces correctly
     """
     expected_output = "https://www.google.com/search?tbm=isch&q=Hello%20World"
     resulted_output = scrapper.create_url("Hello World")
